d46d08033ce2a8f3f60501c952af991095f4a671
hatanoryosuke <r.hatano1986@gmail.com>
2020-01-15 19:52:40 +0900

    メイン　ビュー変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :item
   belongs_to :user
 
-  validates :price,    length: { minimum: 3 }  
+  validates :price,    length: { minimum: 3 }, allow_nil: true	
+  validates :price,    length: { maximum: 6 }
+  validates :text,    length: { maximum: 100 }
 end
