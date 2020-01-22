af7f99c00f829c1bf4df3b89800be7d7b11a0593
hatanoryosuke <r.hatano1986@gmail.com>
2020-01-22 18:48:47 +0900

    JQ 発火しないため一旦、ファイル削除

--- app/assets/javascripts/items_create.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', function () {
-  ityped.init(document.querySelector("#ityped"), {
-    strings: ['Post complete'],
-    typeSpeed: 100,
-    backSpeed: 80,
-    startDelay: 500,
-    loop: true
-  });
-})
