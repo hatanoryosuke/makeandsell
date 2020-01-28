d110fd13f185bb3a3f742b766d24ad8596d94992
hatanoryosuke <r.hatano1986@gmail.com>
2020-01-28 21:33:58 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'makeandsell'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  github.com/hatanoryosuke/makeandsell.git
+set :repo_url,  'github.com/hatanoryosuke/makeandsell.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
